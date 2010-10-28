0a8114bf4dc98df8efc036b3eb05796f5d8ae339
Chris Metcalf <chris@chrismetcalf.net>
2010-10-28 10:15:28 -0700

    A few small changes: - Added Tropo logo - Commented and cleaned up calling script - Small CSS changes

--- public/rb/christmas.rb
+++ public/rb/christmas.rb
@@ -1,3 +1,11 @@
+#!/usr/bin/env ruby
+#
+# A simple example app using the Socrata Open Data API and the Tropo IVR API
+# - http://dev.socrata.com
+# - http://www.tropo.com
+#
+# For more details: <insert blog post link>
+#
 
 require 'net/http'
 require 'uri'
@@ -6,43 +14,60 @@ require 'json'
 # Look up facilities given a zip code and type
 def lookup_facilities(zip_code)
   log "Looking for facilities in zip code #{zip_code}..."
+
+  # This looks complicated, but I'm just building the hash that I'll convert to
+  # JSON in order to run a dynamic filter against the Socrata API.
   query = {
+    # The 4-4 view ID of the ID I want to filter against
     "originalViewId" => "zqwi-c5q3",
+
+    # A temporary name for my filter
     "name" => "What do I do with",
+
+    # The heart of the matter - our filter hash
     "query" => {
-        "filterCondition" => {
+      "filterCondition" => {
+        "type" => "operator",
+
+        # We want to filter on the "AND" of two conditions
+        "value" => "AND",
+        "children" => [ {
+          # The first condition checks that the "Zip Code" column matches the
+          # zip code that was passed in
           "type" => "operator",
-          "value" => "AND",
+          "value" => "EQUALS",
           "children" => [ {
-            "type" => "operator",
-            "value" => "EQUALS",
-            "children" => [ {
-              "columnId" => 2571137,
-              "type" => "column"
-            }, {
-              "type" => "literal",
-              "value" => zip_code
-            } ]
+            "columnId" => 2571137,
+            "type" => "column"
           }, {
-            "type" => "operator",
-            "value" => "EQUALS",
-            "children" => [ {
-              "columnId" => 2571122,
-              "type" => "column"
-            }, {
-              "type" => "literal",
-              "value" => "Yard Waste"
-            } ]
+            "type" => "literal",
+            "value" => zip_code
           } ]
-        }
+        }, {
+          # The second condition filter to only return entries for facilities
+          # that accept "Yard Waste"
+          "type" => "operator",
+          "value" => "EQUALS",
+          "children" => [ {
+            "columnId" => 2571122,
+            "type" => "column"
+          }, {
+            "type" => "literal",
+            "value" => "Yard Waste"
+          } ]
+        } ]
       }
     }
+  }
 
+  # Here we just set up a simple Net::HTTP POST request including our query and
+  # a content-type
   request = Net::HTTP::Post.new("/api/views/INLINE/rows.json?method=index")
   request.body = query.to_json
   request.content_type = "application/json"
   response = Net::HTTP.start("www.datakc.org", 80){ |http| http.request(request) }
 
+  # Check our response code and output our results
   if response.code != "200"
     log "Error code: #{response.code}"
     log "Body: #{response.body}"
@@ -56,17 +81,17 @@ def lookup_facilities(zip_code)
       say results.map{|r| "#{r[9]} at #{r[10]}. Their hours are #{r[15]}." }.join(" or, ")
     end
   end
-
-  log(response.inspect)
 end
 
 answer
 sleep(2)
 say "Welcome to the King County Christmas Tree Recycling Line."
 
+# Setting up our options for "ask". We accept 5 digits, either spoken or entered
+# by DTMF, and time out after 10 seconds.
 zipcode_options = { :choices     => "[5 DIGITS]",
                     :repeat      => 3,
-                    :timeout     => 7,
+                    :timeout     => 10,
                     :onBadChoice => lambda { say 'Invalid entry, please try again.' },
                     :onTimeout   => lambda { say 'Timeout, please try again.' },
                     :onChoice    => lambda { |zip| lookup_facilities(zip.value) }
